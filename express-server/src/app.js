1d8eefec949be892cac15e7825ebfc35ce650252
Tess Gronski <theresegronski@Tesss-MacBook-Air.local>
2019-11-19 22:36:09 -0800

    first commit

---
+++ express-server/src/app.js
@@ -0,0 +1,53 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+const { NODE_ENV } = require('./config')
+const app = express()
+const winston = require('winston');
+
+// const uuid = require('uuid/v4');
+const bookmarksRouter= require('./book/book-router')
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    transports: [
+      new winston.transports.File({ filename: 'info.log' })
+    ]
+  });
+  
+  if (NODE_ENV !== 'production') {
+    logger.add(new winston.transports.Console({
+      format: winston.format.simple()
+    }));
+}
+
+
+// const store = require('../store')
+
+
+
+
+app.use(bookRouter)
+
+
+
+
+  
+const morganOption = (NODE_ENV === 'production')
+  ? 'tiny'
+  : 'common';
+
+app.use(morgan(morganOption))
+app.use(helmet())
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello, world!')
+})
+app.use(bookmarksRouter)
+
+app.use(errorHandler)
+module.exports = app
